913896b1345df6952803e510193d64b27bffa199
akashnai <akashnai632@gmail.com>
2023-02-24 23:53:58 +0530

    video 11

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import Greet from "./components/Greet";
 import Welcome from "./components/Welcome";
 import Hello from "./components/Hello";
 import Message from "./components/Message";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <div className="App">
-      <Message />
-
+      <Counter />
+      {/* <Message /> */}
       {/* <Greet lname="Nai" fname="Akki">
         <p>This is children prop</p>
       </Greet>
