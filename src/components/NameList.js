c8fda6bc41c55d64453282f6d760c88aa4091414
akashnai <akashnai632@gmail.com>
2023-02-25 01:50:41 +0530

    video 19

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -2,19 +2,20 @@ import React from "react";
 import Person from "./Person";
 
 function NameList() {
+  const names = ["Akki", "Keval", "Shruti"];
   const persons = [
     { id: 1, name: "akki", age: 20, skill: "react" },
     { id: 2, name: "keval", age: 21, skill: "Data science" },
     { id: 3, name: "nilay", age: 24, skill: "Java" },
   ];
-  const personList = persons.map((person) => <Person key={person.id} person={person} />);
+  const nameList = names.map((name,index) => <h2 key={index}>{index} {name}</h2>);
   return (
     <div>
       {/* <h2>{names[0]}</h2>
         <h2>{names[1]}</h2>
         <h2>{names[2]}</h2> */}
 
-      <h2>{personList}</h2>
+      {nameList}
     </div>
   );
 }
